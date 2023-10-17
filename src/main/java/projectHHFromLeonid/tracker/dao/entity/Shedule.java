e5bf5ff67b8597aa62109c6e71ea1a77e0e772c2
evgenii volkov <volkov.e.n@gmail.com>
2023-10-17 19:13:36 +0300

    fix imports

--- src/main/java/projectHHFromLeonid/tracker/dao/entity/Shedule.java
+++ src/main/java/projectHHFromLeonid/tracker/dao/entity/Shedule.java
@@ -4,9 +4,16 @@ package projectHHFromLeonid.tracker.dao.entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import projectHHFromLeonid.tracker.dao.entity.Vacancies;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.util.ArrayList;
 import java.util.List;
 @Getter
