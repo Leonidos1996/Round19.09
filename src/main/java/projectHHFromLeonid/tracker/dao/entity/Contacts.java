bcf3f3da72ce8bd79947d306f0a451bb36b29118
Leonidos1996 <l.pesk@yandex.ru>
2023-11-08 17:16:15 +0300

    task-4: add import

--- src/main/java/projectHHFromLeonid/tracker/dao/entity/Contacts.java
+++ src/main/java/projectHHFromLeonid/tracker/dao/entity/Contacts.java
@@ -1,10 +1,7 @@
-
 package projectHHFromLeonid.tracker.dao.entity;
-
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
---
+++ src/main/java/projectHHFromLeonid/tracker/dao/entity/Employer.java
@@ -0,0 +1,48 @@
+
+package projectHHFromLeonid.tracker.dao.entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.HashSet;
+import java.util.Set;
+
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "EMPLOYER")
+public class Employer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    @Column
+    private String id;
+
+    @Column
+    private Boolean accredited_it_employer;
+
+    @Column
+    private String name;
+
+    @Column
+    private Boolean trusted;
+
+    @Column
+    private String url;
+
+
+    @OneToMany(mappedBy = "employer", fetch = FetchType.LAZY)
+    private Set<Vacancy> vacancies = new HashSet<>();
+
+}
